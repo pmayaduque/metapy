027679df284913eab33cf0a4d3640e2ec8d578e7
carlassmann <carlassmann@me.com>
2018-11-12 21:22:07 +0100

    Fixed knapsack example

---
+++ metapy/__init__.py
@@ -0,0 +1,6 @@
+import metapy.algorithms
+import metapy.selection
+import metapy.mutation
+import metapy.initialization
+import metapy.fitness
+import metapy.crossover
--- metapy/doc/examples/knapsack_problem.py
+++ metapy/doc/examples/knapsack_problem.py
@@ -1,45 +1,45 @@
 # TODO Show usage of Genetic algorithm on the example of the Knapsack Problem
 import numpy as np
+import metapy as mp
 from metapy.algorithms import GeneticAlgorithm
 
 
 class KnapsackAlgorithm(GeneticAlgorithm):
     def __init__(self):
-        super().__init__(generations=20, mutation_rate=0.1, selection_size=10, elitism=0, minimize=False)
-        self.population = [np.random.randint(0, 2, 10) for i in range(10)]
-    
+        super().__init__(generations=20, mutation_rate=0.1,
+                         population_size=10, elitism=0, minimize=False)
+        self.population = [np.random.randint(0, 2, 10) for i in range(self.population_size)]
+        self.values = [np.random.randint(0, 10) * 10 for i in range(10)]
+        self.weights = [np.random.randint(0, 10) for i in range(10)]
+
     def fitness(self, candidate):
-        values = [np.random.randint(0, 10) * 10 for i in range(10)]
-        weights = [np.random.randint(0, 10) for i in range(10)]
-
         W = 10
-
         total_weight = 0
         fitness = 0
         for i, bit in enumerate(candidate):
             if bit == 1:
-                total_weight += weights[i]
-                fitness += values[i]
-        
+                total_weight += self.weights[i]
+                fitness += self.values[i]
+
         if total_weight > W:
-            fitness -= 10 * (total_weight - W)
-        
+            fitness -= 3 * (total_weight - W)
+
         return fitness
-    
+
     def crossover(self, candidates):
-        return self.uniform_crossover(candidates)
+        return mp.crossover.uniform_crossover(candidates)
 
     def mutation(self, candidate):
-        return self.bit_flip_mutation(candidate)
-    
+        if np.random.uniform() > self.mutation_rate:
+            return mp.mutation.bit_flip_mutation(candidate)
+        else:
+            return candidate
+
     def selection(self, fitness):
-        return self.rank_based_selection(fitness, number_of_parents=2)
+        return mp.selection.rank_based_selection(self.population, fitness, self.selection_size, number_of_parents=2, minimize=False)
 
 
 def main():
     ga = KnapsackAlgorithm()
-    res = ga.optimize(3)
-    print(res['x'])
-
-if __name__ == '__main__':
-    main()
+    res = ga.optimize()
+    print(res)
